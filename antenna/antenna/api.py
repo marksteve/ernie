2ea4ce0f415057cbd22738f41ebc458e33390fce
Mark Steve Samson <hello@marksteve.com>
2014-10-08 23:58:30 +0800

    Initial echo sms receiver

---
+++ antenna/antenna/api.py
@@ -0,0 +1,54 @@
+import requests
+from flask import current_app
+from flask_restful import abort, fields, marshal_with, Resource
+from flask_restful.reqparse import RequestParser
+from simpleflake import simpleflake
+
+CHIKKA_REPLY_ENDPOINT = "https://post.chikka.com/smsapi/request"
+
+
+class SMS(Resource):
+
+  def __init__(self):
+    parser = RequestParser()
+    parser.add_argument("message_type", type=str)
+    parser.add_argument("mobile_number", type=int)
+    parser.add_argument("shortcode", type=int)
+    parser.add_argument("request_id", type=str)
+    parser.add_argument("message", type=str)
+    parser.add_argument("timestamp", type=int)
+    self.parser = parser
+
+  @marshal_with(dict(
+    status=fields.Integer,
+    message=fields.String,
+  ))
+  def post(self):
+    args = self.parser.parse_args()
+
+    if args.message_type != "incoming":
+      abort(400, message="Unknown message type: {}".format(args.message_type))
+
+    if args.shortcode != int(current_app.config["CHIKKA_SHORTCODE"]):
+      abort(400, message="Invalid shortcode: {}".format(args.shortcode))
+
+    res = requests.post(
+      CHIKKA_REPLY_ENDPOINT,
+      data=dict(
+        message_type="REPLY",  # Inconsistent
+        mobile_number=args.mobile_number,
+        shortcode=args.shortcode,
+        request_id=args.request_id,
+        message_id=simpleflake(),
+        message=args.message,
+        request_cost="FREE",
+        client_id=current_app.config["CHIKKA_CLIENT_ID"],
+        secret_key=current_app.config["CHIKKA_SECRET_KEY"],
+      ),
+    )
+
+    if res.status_code != requests.codes.ok:
+      abort(500)
+
+    return dict(status=200, message=args.message)
+
--- antenna/antenna/app.py
+++ antenna/antenna/app.py
@@ -1,7 +1,18 @@
+import os
+
 from flask import Flask
+from flask_restful import Api
+from .api import SMS
 
 
 def create_app(**config):
   app = Flask(__name__)
   app.config.update(**config)
+  app.config.update(
+    CHIKKA_CLIENT_ID=os.environ["CHIKKA_CLIENT_ID"],
+    CHIKKA_SECRET_KEY=os.environ["CHIKKA_SECRET_KEY"],
+    CHIKKA_SHORTCODE=os.environ["CHIKKA_SHORTCODE"],
+  )
+  api = Api(app)
+  api.add_resource(SMS, "/sms")
   return app
