f1d599add2bae6e3f92b389e8a747b30eadd90ac
Kat <kat.padilla@stellarloyalty.com>
2014-10-31 18:23:14 +0800

    Wolfram working

--- omniscient/app.rb
+++ omniscient/app.rb
@@ -8,24 +8,14 @@ require 'wolfram-alpha'
 configure do
   set :conf, YAML.load_file('config.yml') rescue nil || {}
   set :bind, '0.0.0.0'
+  set :logging, true
+end
+
+before do
+  content_type :json
 end
 
 helpers do
-  def json_status(code, reason)
-    status code
-    {
-      status: code,
-      reason: reason
-    }.to_json
-  end
-
-  def accept_params(params, *fields)
-    h = {}
-    fields.each do |name|
-      h[name] = params[name] if params[name]
-    end
-    h
-  end
 end
 
 get '/' do
@@ -33,16 +23,14 @@ get '/' do
 end
 
 get '/weather/:location' do
-  return json_status 400, 'Missing param' unless params[:location]
-  content_type :json
+  halt 400, 'Missing param' unless params[:location]
   location = params[:location]
   result = []
-  # hsh.each_pair{ |k,v| puts "#{k}: #{v}"}
+
   client = Weatherman::Client.new
   response = client.lookup_by_location("#{location}")
-  if response.description.include? 'Invalid Input'
-    return json_status 404, 'Not found'
-  end
+  halt 404, 'Not found' if response.description.include? 'Invalid Input'
+
   result = {
     temp: response.condition['temp'] || '',
     desc: response.condition['text'] || '',
@@ -52,48 +40,44 @@ get '/weather/:location' do
 end
 
 get '/goto/:origin/:destination' do
-  return json_status 400, 'Missing param' unless params[:destination]
-  content_type :json
+  halt 400, 'Missing param' unless params[:destination]
   origin = params[:origin]
   destination = params[:destination]
-  # If params origin is missing, just give back the "address"
+  # TODO If params origin is missing, just give back the "address"
   directions = GoogleDirections.new(origin, destination)
-  if directions.status != 'OK'
-    return json_status 404, 'Not found'
-  end
+  halt 404, 'Not found' if directions.status != 'OK'
+
   result = {
     distance: directions.distance_text,
     time: directions.drive_time_in_minutes,
-    steps: directions.steps.to_s.gsub(/<[^>]*>/ui, '').gsub(/"|'/, '') }
+    steps: directions.steps.to_s.gsub(/<[^>]*>/ui, '').gsub(/"|'/, ''),
+    size: directions.steps.size }
   result.to_json
 end
 
 # TODO
-get '/movie/:sched' do
-  return json_status 418, 'Teapot!'
+get '/traffic/:location' do
+  halt 418, 'Teapot!'
 end
 
 # TODO
+get '/movie/:sched' do
+  halt 418, 'Teapot!'
+end
+
 get '/any/:whatever' do
-  return json_status 500, 'Missing config' if settings.conf['api']['wolfram_key'].nil?
-  content_type :json
+  halt 500, 'Missing config' if settings.conf['api']['wolfram_key'].blank?
+  halt 400, 'Missing param' unless params[:whatever]
   whatever = params[:whatever]
-
   options = { format: 'plaintext' } # see the reference appendix in the documentation.[1]
-
   client = WolframAlpha::Client.new settings.conf['api']['wolfram_key'], options
-
   response = client.query "#{whatever}"
-  input = response['Input'] # Get the input interpretation pod.
-  puts response.inspect
-  halt 400
-  query_result = response.queryresult
-  return json_status 500, 'API failed' if query_result.success == 'false'
-  return json_status 400, 'Not found' if query_result.error == 'true'
-
-  result = response.find { |pod| pod.title == 'Result' } # Get the result pod.
-  if result.nil?
-    result = response.find { |pod| pod.title == 'Basic information' }
-  end
-  "#{input.subpods[0].plaintext} = #{result.subpods[0].plaintext}"
+  input = response["Input"] # Get the input interpretation pod.
+  result = response.find { |pod| pod.title == 'Result' } # Get the result pods
+  puts result.inspect
+  halt 404, "Not found" if result.nil?
+  result = {
+    query: input.subpods[0].plaintext,
+    answer: result.subpods[0].plaintext }
+  result.to_json
 end
