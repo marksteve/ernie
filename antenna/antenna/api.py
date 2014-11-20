60fd19e1ba7eb4285cf59356393bb4041fd4c0e5
Mark Steve Samson <hello@marksteve.com>
2014-11-20 18:34:59 +0800

    Increase confidence requirement

--- antenna/antenna/api.py
+++ antenna/antenna/api.py
@@ -38,7 +38,7 @@ def ernie_answer(q):
 
   current_app.logger.debug("Outcome: %r", outcome)
 
-  if outcome["confidence"] < 0.7:
+  if outcome["confidence"] < 0.8:
     intent = "wolfram"
 
   if intent == "get_weather":
